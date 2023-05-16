2f42aeeb7eca08a96efb408b51a230623a3f2734
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2023-05-16 18:34:37 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const {server, router} = require('./servidor/server.js');
+
+
+router.get('/db', (req, res) => {
+  const filePath = path.join(__dirname, "db", "db.json");
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(content, 'utf-8');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+  
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
